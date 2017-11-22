8cfd56c7b19651bc05240b675be2e3eb3d91e80c
Erick Banks <erick@puppetlabs.com>
2017-11-21 17:44:42 -0800

    WIP (MODULES-5978) convert acceptance tests to rspec

---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,28 @@
+require 'beaker-rspec'
+require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
+
+run_puppet_install_helper
+
+install_ca_certs
+
+proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+hosts.each do |host|
+  install_dev_puppet_module_on(host, :source => proj_root, :module_name => 'acl')
+end
+
+def target_parent
+  'c:/temp'
+end
+
+def user_id
+  'bob'
+end
+
+def windows_agents
+  agents.select { |agent| agent['platform'].include?('windows') }
+end
+
+
+
+
